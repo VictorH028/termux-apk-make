144ff942d4949d589fb160de7ed0f854cd659766
victorh028 <demonhunternauta@gmail.com>
2026-08-04 13:58:03 +0500

    Remodelacion de el codigo

--- apk-build/src/com/deadnote/MainActivity.java
+++ apk-build/src/com/deadnote/MainActivity.java
@@ -4,23 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.ImageView;
-
+import android.view.Gravity;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ImageView fondo = new ImageView(this);
-        fondo.setImageResource(R.drawable.fondo_app);
-        fondo.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        
-        setContentView(fondo);
- 
-        // setContentView(R.layout.activity_main);
         TextView textView = new TextView(this);
         textView.setText(R.string.text_hello_world);
-        textView.setTextSize(22);
+        textView.setTextSize(24);
         setContentView(textView);
     }
 }
